0c186dd2ea0fc63c21a168d44f9a034b4b8e8e85
nataliiatkachenko <azaria1993@gmail.com>
2021-05-21 20:04:24 +0300

    add Image, Genre entities

---
+++ PhotoAlbum.DAL/Entities/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PhotoAlbum.DAL
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int Rate { get; set; }
+        public int GenreId { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
