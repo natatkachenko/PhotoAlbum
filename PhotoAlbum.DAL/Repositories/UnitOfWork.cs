2d0ba709e9aa7c058b993cbb6cda6e4b82627236
nataliiatkachenko <azaria1993@gmail.com>
2021-05-23 15:23:23 +0300

    added class UnitOfWork as implementation of IUnitOfWork

---
+++ PhotoAlbum.DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using PhotoAlbum.DAL.EFContext;
+using PhotoAlbum.DAL.Entities;
+using PhotoAlbum.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.DAL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        readonly PhotoContext db;
+        private PhotoRepository photoRepository;
+        private GenreRepository genreRepository;
+
+        public UnitOfWork(PhotoContext context)
+        {
+            db = context;
+        }
+
+        public IRepository<Photo> PhotoRepository => photoRepository = photoRepository ?? new PhotoRepository(db);
+
+        public IRepository<Genre> GenreRepository => genreRepository = genreRepository ?? new GenreRepository(db);
+
+        public Task<int> SaveAsync()
+        {
+            return db.SaveChangesAsync();
+        }
+    }
+}
