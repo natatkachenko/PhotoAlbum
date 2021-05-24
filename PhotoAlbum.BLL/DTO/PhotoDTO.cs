10cee078829c18d21d04939e6b3dd4e684dec1c1
nataliiatkachenko <azaria1993@gmail.com>
2021-05-24 13:42:45 +0300

    added DTO iin BLL

---
+++ PhotoAlbum.BLL/DTO/PhotoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoAlbum.BLL.DTO
+{
+    public class PhotoDTO
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int Rate { get; set; }
+        public int GenreId { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
