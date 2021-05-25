640d3d596a27813c1d341d01330eb8a2d09052ad
nataliiatkachenko <azaria1993@gmail.com>
2021-05-25 12:42:34 +0300

    set default value of the Date property in the Photo entity to DateTime.Now

--- PhotoAlbum.DAL/Entities/Photo.cs
+++ PhotoAlbum.DAL/Entities/Photo.cs
@@ -7,7 +7,7 @@ namespace PhotoAlbum.DAL.Entities
         public int Id { get; set; }
         public string FileName { get; set; }
         public string Description { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
         public int Rate { get; set; }
         public int GenreId { get; set; }
         public byte[] Data { get; set; }
