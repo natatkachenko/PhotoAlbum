051933d75e3dddbb688463350783be5d2cb1fad2
nataliiatkachenko <azaria1993@gmail.com>
2021-05-26 18:55:39 +0300

    added Identity and User entity in the DAL

---
+++ PhotoAlbum.DAL/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoAlbum.DAL.Entities
+{
+    public class User : IdentityUser
+    {
+        public DateTime DateOfBirth { get; set; }
+    }
+}
