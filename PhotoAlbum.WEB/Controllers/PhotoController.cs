ff0f86c09f15c1e58c254084da53a74daa66fcd4
nataliiatkachenko <azaria1993@gmail.com>
2021-05-26 14:06:23 +0300

    added PhotoController in the WEB

---
+++ PhotoAlbum.WEB/Controllers/PhotoController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PhotoAlbum.BLL.DTO;
+using PhotoAlbum.BLL.Interfaces;
+
+namespace PhotoAlbum.WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        readonly IService<PhotoDTO> photoService;
+
+        public PhotoController(IService<PhotoDTO> service)
+        {
+            photoService = service;
+        }
+
+        // GET: api/photo
+        [HttpGet]
+        public ActionResult<IEnumerable<PhotoDTO>> GetAll ()
+        {
+            var books = photoService.GetAll();
+            return Ok(books);
+        }
+
+        // GET api/<PhotoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PhotoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PhotoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PhotoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
