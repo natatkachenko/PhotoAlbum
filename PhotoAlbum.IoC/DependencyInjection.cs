46b40891258eacc511af263f1c45fc7adc06d90f
nataliiatkachenko <azaria1993@gmail.com>
2021-05-26 17:33:20 +0300

    added the reference to the BLL for the IoC project, UnitOfWork, Automapper, Repositories and Services dependencies in the DependencyInjection class

--- PhotoAlbum.IoC/DependencyInjection.cs
+++ PhotoAlbum.IoC/DependencyInjection.cs
@@ -1,7 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PhotoAlbum.BLL;
+using PhotoAlbum.BLL.DTO;
+using PhotoAlbum.BLL.Interfaces;
+using PhotoAlbum.BLL.Services;
 using PhotoAlbum.DAL.EFContext;
+using PhotoAlbum.DAL.Entities;
+using PhotoAlbum.DAL.Interfaces;
+using PhotoAlbum.DAL.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -26,6 +34,21 @@ namespace PhotoAlbum.IoC
             }
 
             services.AddDbContext<PhotoContext>(options => options.UseSqlServer(con));
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            var mappingConfig = new MapperConfiguration(mapConfig =>
+            {
+                mapConfig.AddProfile(new AutomapperProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddTransient<IRepository<Photo>, PhotoRepository>();
+            services.AddTransient<IRepository<Genre>, GenreRepository>();
+            services.AddTransient<IService<PhotoDTO>, PhotoService>();
+            services.AddTransient<IService<GenreDTO>, GenreService>();
         }
     }
 }
