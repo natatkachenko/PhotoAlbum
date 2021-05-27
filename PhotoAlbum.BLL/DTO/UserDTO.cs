f4efb0a25ad363aaedd101a8eb0b978edacd5bd3
nataliiatkachenko <azaria1993@gmail.com>
2021-05-27 18:34:34 +0300

    added UserDTO

---
+++ PhotoAlbum.BLL/DTO/UserDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PhotoAlbum.BLL.DTO
+{
+    public class UserDTO
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords don't match!")]
+        [Display(Name = "Confirm password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
