8aa7c8269ca9bcd28e6341687e46837ba64fa556
nataliiatkachenko <azaria1993@gmail.com>
2021-05-27 18:39:57 +0300

    added IUserService in the BLL

---
+++ PhotoAlbum.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using PhotoAlbum.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoAlbum.BLL.Interfaces
+{
+    public interface IUserService : IService<UserDTO>
+    {
+        bool isExist(UserDTO dto);
+    }
+}
