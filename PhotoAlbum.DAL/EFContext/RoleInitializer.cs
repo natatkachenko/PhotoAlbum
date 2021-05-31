ed5e582c2d0cf060e70b1d39e0de1e7992c4b304
nataliiatkachenko <azaria1993@gmail.com>
2021-05-31 18:23:29 +0300

    added class RoleInitializer to seed DB with roles and administrator data

---
+++ PhotoAlbum.DAL/EFContext/RoleInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PhotoAlbum.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoAlbum.DAL.EFContext
+{
+    public class RoleInitializer
+    {
+        public static Initialize(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            
+        }
+    }
+}
