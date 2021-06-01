4112642804b7659e4b8561584ca23c0fb5ff2697
nataliiatkachenko <azaria1993@gmail.com>
2021-06-01 19:39:42 +0300

    implemented getPhotos method in the PhotoService

--- PhotoAlbum.Angular/src/app/services/photo.service.ts
+++ PhotoAlbum.Angular/src/app/services/photo.service.ts
@@ -1,10 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Http2ServerRequest } from 'http2';
+import { Observable } from 'rxjs';
+import { Photo } from '../models/photo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
+  baseUrl="https://localhost:44356/api/";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPhotos(): Observable<Photo[]> {
+    return this.http.get<Photo[]>(this.baseUrl + "photos");
+  }
 }
