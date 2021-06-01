0c2c5406c1f267f33d1f57e223e134663a114b26
nataliiatkachenko <azaria1993@gmail.com>
2021-06-01 19:52:59 +0300

    added Cors middleware in the Startup to allow requests from the Angular application

--- PhotoAlbum.WEB/Startup.cs
+++ PhotoAlbum.WEB/Startup.cs
@@ -34,11 +34,18 @@ namespace PhotoAlbum.WEB
             services.AddControllers();
             DependencyInjection dependencyInjection = new DependencyInjection(Configuration);
             dependencyInjection.AddDependencies(services, AppRootPath);
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
