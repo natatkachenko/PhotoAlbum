a2ceaa8f5eac739e250dc363e5ea4dae76ff1f60
nataliiatkachenko <azaria1993@gmail.com>
2021-06-02 19:52:54 +0300

    removed angular routing

--- PhotoAlbum.Angular/src/app/app.module.ts
+++ PhotoAlbum.Angular/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotosComponent } from './photos/photos.component';
 import { PhotosItemComponent } from './photos/photos-item/photos-item.component';
@@ -13,8 +12,7 @@ import { PhotosItemComponent } from './photos/photos-item/photos-item.component'
     PhotosItemComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
