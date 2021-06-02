18621c37e1324bb560af74e6fd9712c3387949b0
nataliiatkachenko <azaria1993@gmail.com>
2021-06-02 20:29:13 +0300

    changed db migration, fixed errors in the angular components

--- PhotoAlbum.DAL/Migrations/20210602170541_Initial.Designer.cs
+++ PhotoAlbum.DAL/Migrations/20210602170541_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PhotoAlbum.DAL.EFContext;
 
 namespace PhotoAlbum.DAL.Migrations
 {
     [DbContext(typeof(PhotoContext))]
-    partial class PhotoContextModelSnapshot : ModelSnapshot
+    [Migration("20210602170541_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
