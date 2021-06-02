884550598f0113f8beeaeeda75494e35ccc482fb
nataliiatkachenko <azaria1993@gmail.com>
2021-06-02 19:25:56 +0300

    changed PhotoDTO according to the PhotoEntity

--- PhotoAlbum.BLL/DTO/PhotoDTO.cs
+++ PhotoAlbum.BLL/DTO/PhotoDTO.cs
@@ -8,12 +8,11 @@ namespace PhotoAlbum.BLL.DTO
     public class PhotoDTO
     {
         public int Id { get; set; }
-        public string FileName { get; set; }
-        public string Description { get; set; }
+        public string Title { get; set; }
         public DateTime Date { get; set; }
         public int Rate { get; set; }
         public string UserId { get; set; }
-        public byte[] Data { get; set; }
+        public string ImagePath { get; set; }
         public bool isDeleted { get; set; }
     }
 }
