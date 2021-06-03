199ec5fa50f598ce296ea7121c2fb6ccff65ad32
nataliiatkachenko <azaria1993@gmail.com>
2021-06-03 16:38:45 +0300

    added UploadController with implemented photo uploading logic

---
+++ PhotoAlbum.WEB/Controllers/UploadController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhotoAlbum.WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        readonly IWebHostEnvironment _hostingEnvironment;
+
+        public UploadController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpPost]
+        public IActionResult Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                if (file.Length > 0)
+                {
+                    var filePath = Path.Combine(_hostingEnvironment.WebRootPath, "images");
+                    var fileNameWithPath = Path.Combine(filePath, file.FileName);
+                    var dbPath = Path.Combine("images", file.FileName);
+
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    return Ok(new { dbPath });
+                }
+                else
+                    return BadRequest();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
