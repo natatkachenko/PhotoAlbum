b688a428a833e2817a867182f8adeb0368334e69
nataliiatkachenko <azaria1993@gmail.com>
2021-06-04 15:20:40 +0300

    added post request with the emitted event in the UploadComponent

--- PhotoAlbum.Angular/src/app/upload/upload.component.ts
+++ PhotoAlbum.Angular/src/app/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
@@ -9,6 +9,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class UploadComponent implements OnInit {
   public progress: number;
   public message: string;
+  baseUrl = "https://localhost:44356/api/";
   @Output() public uploadFinished = new EventEmitter();
 
   constructor(private http: HttpClient) { }
@@ -16,13 +17,23 @@ export class UploadComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public uploadFile = (files) => {
-    if(files.length === 0) {
+  public uploadFile = (files: string | any[]) => {
+    if(files.length == 0) {
       return;
     }
 
     var fileToUpload = <File>files[0];
     var formData = new FormData();
     formData.append("file", fileToUpload, fileToUpload.name);
+
+    this.http.post(this.baseUrl + "upload", formData, {reportProgress: true, observe: "events"})
+      .subscribe(event => {
+        if(event.type == HttpEventType.UploadProgress)
+          this.progress = Math.round(100 * event.loaded / event.total);
+        else if(event.type == HttpEventType.Response) {
+          this.message = "Image has been uploaded!";
+          this.uploadFinished.emit(event.body);
+        }
+      })
   }
 }
