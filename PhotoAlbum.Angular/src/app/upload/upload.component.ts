c924d1d13ec3f7572a0cdf8812f6c342bcd4e02d
nataliiatkachenko <azaria1993@gmail.com>
2021-06-04 14:46:38 +0300

    added uploadFile function with created FormData object

--- PhotoAlbum.Angular/src/app/upload/upload.component.ts
+++ PhotoAlbum.Angular/src/app/upload/upload.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-upload',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+  public progress: number;
+  public message: string;
+  @Output() public uploadFinished = new EventEmitter();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  public uploadFile = (files) => {
+    if(files.length === 0) {
+      return;
+    }
+
+    var fileToUpload = <File>files[0];
+    var formData = new FormData();
+    formData.append("file", fileToUpload, fileToUpload.name);
+  }
 }
