09770def0e590d1b5d95b996bdaa6db30f27c7be
nataliiatkachenko <azaria1993@gmail.com>
2021-06-06 12:21:10 +0300

    added migration with changed imagePath for the test image, updated database

---
+++ PhotoAlbum.DAL/Migrations/20210606091436_FixedImagePathAccordingToTheImageUpload.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhotoAlbum.DAL.Migrations
+{
+    public partial class FixedImagePathAccordingToTheImageUpload : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImagePath",
+                value: "images\\Forest.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImagePath",
+                value: "images/Forest.jpg");
+        }
+    }
+}
