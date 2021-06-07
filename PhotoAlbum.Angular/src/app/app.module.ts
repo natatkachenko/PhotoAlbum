93172e52b66f19c91f9cbad291319ae5b7d869eb
nataliiatkachenko <azaria1993@gmail.com>
2021-06-08 01:27:38 +0300

    confirmed changes in the app.module

--- PhotoAlbum.Angular/src/app/app.module.ts
+++ PhotoAlbum.Angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PhotosComponent } from './photos/photos.component';
-import { PhotosItemComponent } from './photos/photos-item/photos-item.component';
+import { PhotosItemComponent } from './my-photos/photos-item/photos-item.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UploadComponent } from './upload/upload.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
