fb473764045aacf83ac49f2f5613c49ac141d417
nataliiatkachenko <azaria1993@gmail.com>
2021-06-07 14:00:15 +0300

    added IJWTService

---
+++ PhotoAlbum.BLL/Interfaces/IJWTService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PhotoAlbum.BLL.Interfaces
+{
+    public interface IJWTService
+    {
+        SigningCredentials GetSigningCredentials();
+        List<Claim> GetClaims(IdentityUser user);
+        JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims);
+    }
+}
