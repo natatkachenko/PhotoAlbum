1825305e6279abc0726d420f80cf885ef500ee69
nataliiatkachenko <azaria1993@gmail.com>
2021-06-07 23:41:47 +0300

    implemented GetUserByUserName in the BLL

--- PhotoAlbum.BLL/Interfaces/IUserService.cs
+++ PhotoAlbum.BLL/Interfaces/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using PhotoAlbum.BLL.DTO;
+using PhotoAlbum.DAL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,6 @@ namespace PhotoAlbum.BLL.Interfaces
     public interface IUserService : IService<UserToRegisterDTO>
     {
         bool isExist(UserToRegisterDTO dto);
+        User GetUserByUserName(string userName);
     }
 }
