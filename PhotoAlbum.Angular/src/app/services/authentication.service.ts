82477b16edd3b3ad39811e6f7e04e66cb74450fd
nataliiatkachenko <azaria1993@gmail.com>
2021-06-07 16:44:18 +0300

    added method for notifying about the authentication state change in the Angular

--- PhotoAlbum.Angular/src/app/services/authentication.service.ts
+++ PhotoAlbum.Angular/src/app/services/authentication.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { LoginResponseDTO } from '../models/user/login-response-dto';
+import { Subject } from 'rxjs';
 import { UserToLoginDTO } from '../models/user/user-to-login-dto';
 import { UserToRegisterDTO } from '../models/user/user-to-register-dto';
 import { EnvironmentUrlService } from './environment-url.service';
@@ -10,6 +9,8 @@ import { EnvironmentUrlService } from './environment-url.service';
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private _authChangeSub = new Subject<boolean>()
+  public authChanged = this._authChangeSub.asObservable();
 
   constructor(private _http: HttpClient, private _envUrl: EnvironmentUrlService) { }
 
@@ -19,6 +20,10 @@ export class AuthenticationService {
 
   public loginUser(route: string, body: UserToLoginDTO) {
     return this._http.post(this.createCompleteRoute(route, this._envUrl.urlAddress), body);
+  }
+
+  public sendAuthStateChangeNotification(isAuthenticated: boolean) {
+    this._authChangeSub.next(isAuthenticated);
   }
 
   private createCompleteRoute(route: string, envAddress: string) {
