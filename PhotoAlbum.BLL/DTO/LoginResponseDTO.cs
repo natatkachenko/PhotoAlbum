8095b3c50a9350cb702315d3c1207be1eee603cf
nataliiatkachenko <azaria1993@gmail.com>
2021-06-07 13:54:41 +0300

    added LoginResponseDTO

---
+++ PhotoAlbum.BLL/DTO/LoginResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoAlbum.BLL.DTO
+{
+    public class LoginResponseDTO
+    {
+        public bool IsAuthSuccessful { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Token { get; set; }
+    }
+}
