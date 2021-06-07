3df1d8d0cb3823b155516a1b224b827399c258e4
nataliiatkachenko <azaria1993@gmail.com>
2021-06-07 14:17:58 +0300

    implemented generation of signing credentials, claims and token options in the JWTService

---
+++ PhotoAlbum.BLL/Services/JWTService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using PhotoAlbum.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PhotoAlbum.BLL.Services
+{
+    public class JWTService : IJWTService
+    {
+        readonly IConfiguration configuration;
+        readonly IConfigurationSection jwtSettings;
+
+        public JWTService(IConfiguration conf)
+        {
+            configuration = conf;
+            jwtSettings = conf.GetSection("JwtSettings");
+        }
+
+        public JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var tokenOptions = new JwtSecurityToken(
+            issuer: jwtSettings.GetSection("validIssuer").Value,
+            audience: jwtSettings.GetSection("validAudience").Value,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings.GetSection("expiryInMinutes").Value)),
+            signingCredentials: signingCredentials);
+
+            return tokenOptions;
+        }
+
+        public List<Claim> GetClaims(IdentityUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+            return claims;
+        }
+
+        public SigningCredentials GetSigningCredentials()
+        {
+            var key = Encoding.UTF8.GetBytes(jwtSettings.GetSection("securityKey").Value);
+            var secret = new SymmetricSecurityKey(key);
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+    }
+}
