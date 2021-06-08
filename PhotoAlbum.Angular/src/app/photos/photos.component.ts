b2cab39b3241e8ec1e96f217aaed55afe6b0894f
nataliiatkachenko <azaria1993@gmail.com>
2021-06-08 09:19:12 +0300

    fixed photosComponent to show all photos

--- PhotoAlbum.Angular/src/app/photos/photos.component.ts
+++ PhotoAlbum.Angular/src/app/photos/photos.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Photo } from '../models/photo';
-import { PhotoToCreate } from '../models/photo-to-create';
-import { PhotoToUpdate } from '../models/photo-to-update';
+import { EnvironmentUrlService } from '../services/environment-url.service';
 import { PhotoService } from '../services/photo.service';
 
 @Component({
@@ -11,74 +10,18 @@ import { PhotoService } from '../services/photo.service';
 })
 export class PhotosComponent implements OnInit {
   public photos: Photo[];
-  public title: string;
-  public photoToCreate: PhotoToCreate;
-  public photoToUpdate: PhotoToUpdate;
-  public isCreate: boolean;
-  public isUpdate: boolean;
-  
-  public response: {dbPath: ''};
 
-  constructor(private photoService: PhotoService) { }
+  constructor(private photoService: PhotoService, private envUrl: EnvironmentUrlService) {}
 
   ngOnInit(): void {
     this.getPhotosDetails();
-    this.isCreate = false;
-    this.isUpdate = false;
   }
 
   getPhotosDetails() {
     this.photoService.getPhotosDetails().subscribe(result => this.photos = result);
   }
 
-  addPhotoDetails() {
-    this.photoToCreate = {
-      title: this.title,
-      imagePath: this.response.dbPath
-    }
-    this.photoService.postPhotoDetails(this.photoToCreate).subscribe(() => {
-      this.getPhotosDetails();
-      this.isCreate = false;
-    });
-  }
-
-  goToCreate() {
-    this.isCreate = true;
-    this.title = "";
-  }
-
-  uploadFinished(event: any) {
-    this.response = event;
-  }
-
-  cancelAdd() {
-    this.isCreate = false;
-  }
-
-  getPhotoDetailsByIndex(index: number) {
-    this.photoService.getPhotosDetailsById(index).subscribe(result=> {
-      this.photoToUpdate = result;
-      this.isUpdate = true;
-    });
-  }
-  
-  updatePhotoDetails() {
-    this.photoToUpdate = {
-      id: this.photoToUpdate.id,
-      title: this.title,
-      imagePath: this.photoToUpdate.imagePath
-    }
-    this.photoService.putPhotoDetails(this.photoToUpdate).subscribe(() =>{
-      this.getPhotosDetails();
-      this.isUpdate = false;
-    })
-  }
-
-  cancelUpdate() {
-    this.isUpdate = false;
-  }
-
-  deletePhoto(index: number) {
-    this.photoService.deletePhoto(index).subscribe(()=> this.getPhotosDetails());
+  createImagePath(path: string) {
+    return `${this.envUrl.urlAddress + "/" + path}`;
   }
 }
